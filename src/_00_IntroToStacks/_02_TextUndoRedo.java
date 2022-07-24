aafba9cc6e7db233256b6271fa446173f7957239
amyruyuyan <41204932+amyruyuyan@users.noreply.github.com>
2022-07-24 14:20:09 -0700

    Update _02_TextUndoRedo.java

--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -1,10 +1,14 @@
 package _00_IntroToStacks;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class _02_TextUndoRedo {
+public class _02_TextUndoRedo implements KeyListener {
     /* 
      * Create a JFrame with a JPanel and a JLabel.
      * 
@@ -21,18 +25,58 @@ public class _02_TextUndoRedo {
      * the JLabel.
      */
 
-	public static void main(String[] args) {
-	
 	JFrame frame = new JFrame();
+	
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
+	Stack <String> letters = new Stack();
 	
-	frame.add(panel);
-	frame.add(label);
-
-	
-	if ()
-	
+	public static void main(String[] args) {
+		new _02_TextUndoRedo().setup();
 }
-	public stait
+	
+	void setup(){
+
+		frame.setVisible(true);
+		
+		frame.add(panel);
+		panel.add(label);
+		frame.addKeyListener(this);
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		String letter = label.getText() + e.getKeyChar();
+		if (e.getKeyCode() != 8 && e.getKeyCode() != 10) {
+		label.setText(letter);
+		}
+		
+		if (e.getKeyCode() == 8) {
+		letters.push(label.getText().substring(label.getText().length()-1));
+		String backspace = label.getText().substring(0, label.getText().length()-1);
+		label.setText(backspace);
+		}
+		
+		if (e.getKeyCode() == 10 && !letters.isEmpty()) {
+		String enter = label.getText() + letters.pop();
+		label.setText(enter);
+		}
+		
+		frame.pack();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }

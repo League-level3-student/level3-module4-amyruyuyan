309b3c38c48a43038981645f0137c03053fce74b
amyruyuyan <41204932+amyruyuyan@users.noreply.github.com>
2022-07-31 14:27:35 -0700

    Update TestMatchingBrackets.java

--- src/_01_TestMatchingBrackets/TestMatchingBrackets.java
+++ src/_01_TestMatchingBrackets/TestMatchingBrackets.java
@@ -1,12 +1,41 @@
 package _01_TestMatchingBrackets;
 
+import java.util.Stack;
+
 public class TestMatchingBrackets {
     /*
      * Use a Stack to complete the method for checking if every opening bracket
      * has a matching closing bracket
      */
+	
+	
+	
+	
+	
     public static boolean doBracketsMatch(String b) {
+    	Stack <String> brackets = new Stack();
 
-        return false;
+    	for (int i = 0; i < b.length(); i++) {
+    		if (b.charAt(i) == '{') {
+    			brackets.push("{");
+    		}
+    		if (b.charAt(i) == '}') {
+    			if (brackets.isEmpty()) {
+    				return false;
+    			}
+    			brackets.pop();
+    		}
+    	} 
+    	
+    	if (brackets.isEmpty()) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
+
+    	
     }
+    
+
 }
